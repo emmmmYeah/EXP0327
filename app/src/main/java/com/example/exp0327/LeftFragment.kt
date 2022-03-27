fd796b19bea35f40b64d5642aba702d2d3b77107
emmmmYeah <1776283041@qq.com>
2022-03-27 22:45:35 +0800

    基本完成页面设置，刚开始进行导航条的功能实现

---
+++ app/src/main/java/com/example/exp0327/LeftFragment.kt
@@ -0,0 +1,41 @@
+package com.example.exp0327
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+
+class LeftFragment : Fragment(), View.OnClickListener {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_left,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.btn_life).setOnClickListener(this)
+        view.findViewById<Button>(R.id.btn_study).setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+
+        val act=requireActivity() as MainActivity
+
+        when(v.id){
+            R.id.btn_life->{
+                act.showlife()
+            }
+            R.id.btn_study->{
+                act.showstudy()
+            }
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/exp0327/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.exp0327
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun showlife() {
+        TODO("Not yet implemented")
+    }
+
+    fun showstudy() {
+        TODO("Not yet implemented")
+    }
+}
